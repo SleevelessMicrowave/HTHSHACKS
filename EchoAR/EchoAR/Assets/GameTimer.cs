41208798c33951ec74b5d5b5e88d7052ad303e94
SleevelessMicrowave <62040052+SleevelessMicrowave@users.noreply.github.com>
2020-05-17 00:17:25 -0400

    sped up animation

---
+++ EchoAR/EchoAR/Assets/GameTimer.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.SceneManagement;
+using UnityEngine;
+
+public class GameTimer : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartCoroutine(videoLength());
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    IEnumerator videoLength()
+    {
+        yield return new WaitForSeconds(120);
+        SceneManager.LoadScene("End Screen");
+    }
+}
